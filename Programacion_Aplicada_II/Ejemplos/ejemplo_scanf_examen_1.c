d53c7ab86e301144d7ed1a6c35393dd8907e6353
Erick Agrazal <erick.agrazal@gmail.com>
2017-04-25 09:29:03 -0500

    Added comments to understand the code

--- Programacion_Aplicada_II/Ejemplos/ejemplo_scanf_examen_1.c
+++ Programacion_Aplicada_II/Ejemplos/ejemplo_scanf_examen_1.c
@@ -12,10 +12,17 @@ int main() {
   printf("Leer cantidad de estudiantes: ");
 
   scanf(" %i", &count_estudiantes);
+  // ¡Hack!, para evitar que el `enter` que se de en el teclado
+  // se propague hacia el siguiente scanf, gets o fgets de la aplicación.
   while ((ch = getchar()) != '\n' && ch != EOF);
 
   for (int i = 0; i < count_estudiantes; i++) {
     printf("Imprime el #%d: ", i+1);
+    
+    // fgets(variable, tamaño de la variable, entrada);
+    // stdin, es el input stream (entrada)
+    // Más información: 
+    // http://www.dummies.com/programming/c/how-to-use-the-fgets-function-for-text-input-in-c-programming/
     fgets(nombre[i], sizeof(nombre[i]), stdin);
   }
 
