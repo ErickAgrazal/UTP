4f3fec64892e83ca21b2f5554510e0b95646506f
Erick Agrazal <erick.agrazal@gmail.com>
2017-06-26 21:09:17 -0500

    Update ejemplo_8.c

--- Programacion_Aplicada_II/Ejemplos/ejemplo_8.c
+++ Programacion_Aplicada_II/Ejemplos/ejemplo_8.c
@@ -1,5 +1,5 @@
 
-// Nombre: ejemplo_7.c
+// Nombre: ejemplo_8.c
 // Por: Erick Agrazal
 // Descripción: Escribir un programa que lea 1 año. Imprima si el año introducido es bisiesto.
 //              Imprimir:
