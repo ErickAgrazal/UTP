ef1fc234269df4b8345c4aafd399cbbb5d43d6a3
Erick Agrazal <erick.agrazal@gmail.com>
2017-06-27 07:55:47 -0500

    Update ejemplo_11.c

--- Programacion_Aplicada_II/Ejemplos/ejemplo_11.c
+++ Programacion_Aplicada_II/Ejemplos/ejemplo_11.c
@@ -1,7 +1,7 @@
 
 // Nombre: ejemplo_11.c
 // Por: Erick Agrazal
-// Descripción: Escribir un programa que lea números de una matriz NxN, donde esta definido entre 0 < N <= 5.
+// Descripción: Escribir un programa que lea números de una matriz NxN, donde N esta definido entre 0 < N <= 5.
 //              Calcule e Imprima:
 //                  - `[A] = La suma de la diagonal principal de la matriz.`
 //                  - `[B] = La suma de la diagonal secundaria de la matriz.`
