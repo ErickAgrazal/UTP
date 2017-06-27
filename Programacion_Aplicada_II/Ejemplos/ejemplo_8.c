5cc50c57fac9fbbe29b74eeb01862c9e7ea40c6b
Erick Agrazal <erick.agrazal@gmail.com>
2017-06-26 21:08:44 -0500

    Update ejemplo_8.c

--- Programacion_Aplicada_II/Ejemplos/ejemplo_8.c
+++ Programacion_Aplicada_II/Ejemplos/ejemplo_8.c
@@ -1,1 +1,7 @@
-# Año bisiesto
+
+// Nombre: ejemplo_7.c
+// Por: Erick Agrazal
+// Descripción: Escribir un programa que lea 1 año. Imprima si el año introducido es bisiesto.
+//              Imprimir:
+//                  - `El <año> es bisiesto.`, ó
+//                  - `El <año> no es bisiesto.`
